ac977d3e99064232b0db8eea31a9c7b75b2330d0
PumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2019-12-28 11:22:59 +0800

    Create mergetwouplinklist.cs

---
+++ mergetwouplinklist.cs
@@ -0,0 +1,20 @@
+// 合并两个单调递增的链表，输出合并后单调递增的链表
+
+/*
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode (int x)
+    {
+        val = x;
+    }
+}*/
+class Solution
+{
+    public ListNode Merge(ListNode pHead1, ListNode pHead2)
+    {
+        // write code here
+        
+    }
+}
