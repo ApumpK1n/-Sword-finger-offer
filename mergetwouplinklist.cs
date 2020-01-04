4560736fbccaef2a0f429c8ccef0223f0b758818
PumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-01-04 10:22:30 +0800

    Update mergetwouplinklist.cs

--- mergetwouplinklist.cs
+++ mergetwouplinklist.cs
@@ -15,6 +15,17 @@ class Solution
     public ListNode Merge(ListNode pHead1, ListNode pHead2)
     {
         // write code here
-        
+        if (pHead1 == null) return pHead2;
+        if (pHead2 == null) return pHead1;
+        if (pHead1.val <= pHead2.val)
+        {
+            pHead1.next = Merge(pHead1.next, pHead2);
+            return pHead1;
+        }
+        else 
+        {
+            pHead2.next = Merge(pHead1, pHead2.next);
+            return pHead2;
+        }
     }
 }
