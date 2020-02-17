6db5a7bb0b520557be3a8e6ef07cd02a5578db7d
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-02-17 20:51:24 +0800

    update

---
+++ PrintMinNumber.cs
@@ -0,0 +1,17 @@
+//把数组排成最小的数
+
+class Solution
+{
+    public string PrintMinNumber(int[] numbers)
+    {
+        // write code here
+    }
+
+
+}
+
+
+public static bool Main(string[] args)
+{
+
+}
